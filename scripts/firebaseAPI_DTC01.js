8e92cc3600ed58588db686001afd796191e450f1
Patricia Lo <pkwlo1992@gmail.com>
2023-10-30 01:35:57 -0700

    Add login.html and main.html

---
+++ scripts/firebaseAPI_DTC01.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyCEd2jl4BSFcPCPNJRdVb2DpBxbA7jixWc",
+    authDomain: "comp1800-dtc01.firebaseapp.com",
+    projectId: "comp1800-dtc01",
+    storageBucket: "comp1800-dtc01.appspot.com",
+    messagingSenderId: "91527565419",
+    appId: "1:91527565419:web:1bd113bf465325d2b0212e"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
