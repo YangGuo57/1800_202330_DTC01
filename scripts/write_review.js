6b03467f08c610204dcf6ffb881997bf2ff52650
Yang Guo <yguo57@my.bcit.ca>
2023-11-05 14:20:23 -0800

    add review messages to firebase

---
+++ scripts/write_review.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const submitButton = document.querySelector('.add-review button');
+  submitButton.addEventListener('click', () => {
+    const cleanlinessRatings = getRatings('rating1');
+    const odourRatings = getRatings('rating2');
+    const safenessRatings = getRatings('rating3');
+    const accessibleRatings = getRatings('rating4');
+    const comment = document.getElementById('floatingTextarea2').value;
+
+    // Create a new review object with individual ratings
+    const review = {
+      cleanlinessRatings: cleanlinessRatings,
+      odourRatings: odourRatings,
+      safenessRatings: safenessRatings,
+      accessibleRatings: accessibleRatings,
+      comment: comment,
+    };
+
+    // Add the review to Firestore
+    db.collection('reviews')
+      .add(review)
+      .then((docRef) => {
+        console.log('Review added with ID: ', docRef.id);
+        // Optionally, you can display a success message to the user here.
+      })
+      .catch((error) => {
+        console.error('Error adding review: ', error);
+        // Optionally, you can display an error message to the user here.
+      });
+  });
+});
+
+function getRatings(radioGroupName) {
+  const selectedRadios = document.querySelectorAll(`input[name="${radioGroupName}"]:checked`);
+  return Array.from(selectedRadios, (radio) => parseInt(radio.value));
+}
