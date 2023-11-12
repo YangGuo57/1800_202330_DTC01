449e6780c8aaf158623a9a455bd2e3b157de4dca
Yang Guo <yguo57@my.bcit.ca>
2023-11-11 23:35:08 -0800

    edit add review

--- scripts/write_review.js
+++ scripts/write_review.js
@@ -7,28 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
     const accessibleRatings = getRatings('rating4');
     const comment = document.getElementById('floatingTextarea2').value;
 
-    // Create a new review object with individual ratings
-    const review = {
-      cleanlinessRatings: cleanlinessRatings,
-      odourRatings: odourRatings,
-      safenessRatings: safenessRatings,
-      accessibleRatings: accessibleRatings,
-      comment: comment,
-    };
+    const currentUser = firebase.auth().currentUser;
 
-    // Add the review to Firestore
-    db.collection('reviews')
-      .add(review)
-      .then((docRef) => {
-        console.log('Review added with ID: ', docRef.id);
-      })
-      .catch((error) => {
-        console.error('Error adding review: ', error);
-      });
+    if (currentUser) {
+      const userId = currentUser.uid;
+      const userName = currentUser.displayName || "Unknown"; // Use displayName if available, otherwise default to "Unknown"
+
+      // Add the review to Firestore
+      db.collection('reviews')
+        .add({
+          userId: userId,
+          userName: userName,
+          cleanlinessRatings: cleanlinessRatings.join(','),
+          odourRatings: odourRatings.join(','),
+          safenessRatings: safenessRatings.join(','),
+          accessibleRatings: accessibleRatings.join(','),
+          comment: comment,
+        })
+        .then((docRef) => {
+          console.log('Review added with ID: ', docRef.id);
+        })
+        .catch((error) => {
+          console.error('Error adding review: ', error);
+        });
+    } else {
+      console.error('User not logged in');
+    }
   });
 });
 
 function getRatings(radioGroupName) {
   const selectedRadios = document.querySelectorAll(`input[name="${radioGroupName}"]:checked`);
-  return Array.from(selectedRadios, (radio) => parseInt(radio.value));
+  return Array.from(selectedRadios, (radio) => radio.value);
 }
