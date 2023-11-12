65cfb787f7d7c1548db2ad752d4f89ec1ae6be9a
Yang Guo <yguo57@my.bcit.ca>
2023-11-12 00:39:12 -0800

    add dynamic progress rating bar

--- scripts/review.js
+++ scripts/review.js
@@ -1,52 +1,3 @@
-function displayReviews() {
-  const reviewsContainer = document.getElementById('reviewsContainer');
-
-  const db = firebase.firestore();
-
-  db.collection('reviews')
-    .get()
-    .then((querySnapshot) => {
-
-      querySnapshot.forEach((doc) => {
-        const reviewData = doc.data();
-
-        const reviewElement = document.createElement('div');
-        reviewElement.classList.add('col-md-4', 'mb-4', 'text-center', 'review');
-
-        const starsHtml = generateStarRating(reviewData.rating);
-
-        reviewElement.innerHTML = `
-          <div class="card">
-            <div class="card-body py-01 mt-0">
-              <div class="d-flex align-items-left mb-">
-                <div class="user-icon"><img src="${reviewData.userIcon}" class="rounded-circle shadow-1-strong" width="100" height="100" /></div>
-                <div class="d-flex flex-column ">
-                  <p class="name">${reviewData.userName}</p>
-                  <div class="stars-days">
-                    <ul class="list-unstyled d-flex">
-                      ${starsHtml}
-                    </ul>
-                    <div class="days">
-                      <p>Posted ${reviewData.daysAgo} days ago</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <p class="mb-2">
-                <i class="fas fa-quote-left pe-2"></i>${reviewData.comment}
-              </p>
-            </div>
-          </div>
-        `;
-
-        reviewsContainer.appendChild(reviewElement);
-      });
-    })
-    .catch((error) => {
-      console.error('Error fetching reviews: ', error);
-    });
-}
-
 function generateStarRating(rating) {
   const fullStars = Math.floor(rating);
   const halfStar = rating % 1 >= 0.5;
@@ -61,7 +12,6 @@ function generateStarRating(rating) {
     starsHtml += '<i class="fas fa-star-half-alt text-info"></i>';
   }
 
-  return starsHtml;
 }
 
 function displayAverageRating() {
@@ -92,10 +42,145 @@ function displayAverageRating() {
     });
 }
 
-const mainStarContainer = document.getElementById('main-star');
-mainStarContainer.innerHTML = generateStarRating();
+function displayReviews() {
+  const reviewsContainer = document.getElementById('reviewsContainer');
+
+  const db = firebase.firestore();
+
+  db.collection('reviews')
+    .get()
+    .then((querySnapshot) => {
+
+      querySnapshot.forEach((doc) => {
+        const reviewData = doc.data();
+
+        const reviewElement = document.createElement('div');
+        reviewElement.classList.add('col-md-4', 'mb-4', 'text-center', 'review');
+
+        const starsHtml = generateStarRating(reviewData.rating);
+
+        reviewElement.innerHTML = `
+          <div class="card">
+            <div class="card-body py-01 mt-0">
+              <div class="d-flex align-items-left mb-">
+                <div class="user-icon"><img src="${reviewData.userIcon}" class="rounded-circle shadow-1-strong" width="100" height="100" /></div>
+                <div class="d-flex flex-column ">
+                  <p class="name">${reviewData.userName}</p>
+                  <div class="stars-days">
+                    <div class="days">
+                      <p>Posted ${reviewData.daysAgo} days ago</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <p class="mb-2">
+                <i class="fas fa-quote-left pe-2"></i>${reviewData.comment}
+              </p>
+            </div>
+          </div>
+        `;
+
+        reviewsContainer.appendChild(reviewElement);
+      });
+
+      // Add this line to update dynamic progress bars
+      displayDynamicProgressBars(querySnapshot);
+    })
+    .catch((error) => {
+      console.error('Error fetching reviews: ', error);
+    });
+}
+
+function generateStarRating(rating) {
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 >= 0.5;
+
+  let starsHtml = '';
+
+  for (let i = 0; i < fullStars; i++) {
+    starsHtml += '<i class="fas fa-star text-info"></i>';
+  }
+
+  if (halfStar) {
+    starsHtml += '<i class="fas fa-star-half-alt text-info"></i>';
+  }
+
+  return starsHtml;
+}
+
+// New function to calculate average rating for each category
+function calculateAverageRating(querySnapshot, category) {
+  let totalRating = 0;
+  let reviewCount = 0;
+
+  querySnapshot.forEach((doc) => {
+    const reviewData = doc.data();
+
+    // Get the rating for the specified category
+    const categoryRating = parseFloat(reviewData[`${category.toLowerCase()}Ratings`]);
+
+    totalRating += categoryRating;
+    reviewCount++;
+  });
+
+  // Return the average rating for the category
+  return (totalRating / reviewCount);
+}
+
+// New function to get rating color based on value
+function getRatingColor(value) {
+  if (value >= 75) {
+    return 'danger';
+  } else if (value >= 50) {
+    return 'warning';
+  } else if (value >= 25) {
+    return 'info';
+  } else {
+    return 'success';
+  }
+}
+
+// New function to display dynamic progress bars
+function displayDynamicProgressBars(querySnapshot) {
+  const progressContainer = document.querySelector('.review-progress');
+
+  const ratingItems = ['Cleanliness', 'Odour', 'Safeness', 'Accessible'];
+
+  ratingItems.forEach((item) => {
+    const reviewItem = document.createElement('div');
+    reviewItem.classList.add('review-item');
+
+    const reviewPair = document.createElement('div');
+    reviewPair.classList.add('review-pair');
+
+    const reviewTitle = document.createElement('div');
+    reviewTitle.classList.add('review-title');
+    reviewTitle.innerHTML = `<p>${item}</p>`;
+
+    const reviewBar = document.createElement('div');
+    reviewBar.classList.add('review-bar');
+
+    const progressBar = document.createElement('div');
+    progressBar.classList.add('progress');
+    progressBar.setAttribute('role', 'progressbar');
+    progressBar.setAttribute('aria-label', `${item} example`);
+    progressBar.setAttribute('aria-valuemin', '0');
+    progressBar.setAttribute('aria-valuemax', '100');
+
+    const progressInnerBar = document.createElement('div');
+    progressInnerBar.classList.add('progress-bar', `bg-${getRatingColor(calculateAverageRating(querySnapshot, item))}`);
+    progressInnerBar.style.width = `${calculateAverageRating(querySnapshot, item)}%`;
+
+    progressBar.appendChild(progressInnerBar);
+    reviewBar.appendChild(progressBar);
+    reviewPair.appendChild(reviewTitle);
+    reviewPair.appendChild(reviewBar);
+    reviewItem.appendChild(reviewPair);
+    progressContainer.appendChild(reviewItem);
+  });
+}
 
 document.addEventListener('DOMContentLoaded', function () {
-  displayAverageRating();
   displayReviews();
+  displayAverageRating();
 });
