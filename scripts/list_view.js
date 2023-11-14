c37774d3704128e04879007d74703e047d172387
SHAUN SY <shaunmarcosy@gmail.com>
2023-11-14 13:41:22 -0800

    resolved merge conflict

--- scripts/list_view.js
+++ scripts/list_view.js
@@ -115,7 +115,7 @@ function displayReviewsForToilet(docID) {
         // Populate the review element with review data and star rating
         reviewElement.innerHTML = `
           <h3>${reviewData.userName}</h3>
-          ${starRating}-
+          ${starRating}
           <p>${reviewData.comment}</p>
         `;
 
@@ -134,54 +134,3 @@ displayReviewsForToilet(ID)
 // document.getElementById("favourite-button").addEventListener("click", function () {
 //     this.classList.toggle("favorited");
 // });
-//Shows a list view of all toilets
-function displayCardsDynamically(collection) {
-    let cardTemplate = document.getElementById("toiletCardTemplate"); // Retrieve the HTML element with the ID "toiletCardTemplate" and store it in the cardTemplate variable. 
-
-    db.collection(collection).get()
-        .then(allToilets => {
-            allToilets.forEach(doc => { 
-                var title = doc.data().name;
-                var location = doc.data().geo_local_area;
-                var disability = doc.data().wheel_access;
-                var docID = doc.id;
-                let newcard = cardTemplate.content.cloneNode(true); 
-
-                //update title and text and image
-                newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-location').innerHTML = "Location: " + location;
-                newcard.querySelector('.card-accessibility').innerHTML = "Wheelchair Access: " + disability;
-                newcard.querySelector('a').href = "toilet.html?docID=" + docID;
-                document.getElementById(collection + "-go-here").appendChild(newcard);
-            })
-        })
-}
-displayCardsDynamically("toilets"); 
-
-// FOR SHOWING TOILET READ MORE INFO
-function displayToiletInfo() {
-    let params = new URL(window.location.href); //get URL of search bar
-    let ID = params.searchParams.get("docID"); //get value for key "id"
-    console.log(ID);
-
-    db.collection("toilets")
-        .doc(ID)
-        .get()
-        .then(doc => {
-            thisToilet = doc.data();
-            toiletName = doc.data().name;
-            toiletLocation = doc.data().geo_local_area;
-            toiletAddress = doc.data().address;
-            toiletWheelchair = doc.data().wheel_access;
-            toiletSummer = doc.data().summer_hours;
-            toiletWinter = doc.data().winter_hours;
-            toiletType = doc.data().type;
-
-    
-            document.getElementById("toiletName").innerHTML = toiletName;
-            document.getElementById("details-go-here").innerHTML = toiletLocation + "<br>" + toiletAddress + "<br>" + toiletType + "<br>" + "Wheelchair Access: " + toiletWheelchair + "<br>" + "Summer Hours: " + toiletSummer + "<br>" + "Winter Hours: " + toiletWinter;
-
-        });
-}
-displayToiletInfo();
-
