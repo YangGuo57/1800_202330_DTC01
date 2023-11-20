7deb9d99e262b8e8b130d7d325c5a05597ff96d9
ShaunMLS <shaunmarcosy@gmail.com>
2023-11-20 08:54:50 -0800

    added recent reviews to toilets page

--- scripts/toilet_read_more.js
+++ scripts/toilet_read_more.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
 // FOR SHOWING TOILET READ MORE INFO
 function displayToiletInfo() {
     let params = new URL(window.location.href); //get URL of search bar
@@ -23,3 +24,59 @@ function displayToiletInfo() {
         });
 }
 displayToiletInfo();
+
+function displayReviews(toiletID) {
+    const reviewsTemplate = document.getElementById("reviewTemplate");
+    const reviewsContainer = document.getElementById("reviewsContainer");
+
+    db.collection("reviews")
+        .where("toiletID", "==", toiletID)
+        .limit(3) // Fetch only the first 3 reviews
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                const review = doc.data();
+                const newReview = reviewsTemplate.content.cloneNode(true);
+
+                // Calculate average rating for this review
+                const averageRating = calculateAverageRating(review);
+                const starRating = getStarRating(averageRating);
+
+                // Populate the template with data
+                newReview.querySelector('.review-user').textContent = review.userName;
+                newReview.querySelector('.review-rating').textContent = starRating; // Display star rating
+                newReview.querySelector('.review-comment').textContent = review.comment;
+
+                reviewsContainer.appendChild(newReview);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching reviews:", error);
+        });
+}
+
+function calculateAverageRating(review) {
+    const total = parseInt(review.accessibleRatings) + parseInt(review.cleanlinessRatings) +
+                  parseInt(review.odourRatings) + parseInt(review.safenessRatings);
+    return total / 4; // Assuming there are 4 rating categories
+}
+
+function getStarRating(rating) {
+    let stars = "";
+    for (let i = 1; i <= 5; i++) {
+        stars += i <= rating ? "★" : "☆"; // Full star for each whole number rating
+    }
+    return stars;
+}
+
+function getToiletIdFromUrl() {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get('docID'); // "docID" is the name of the query parameter
+}
+
+const toiletID = getToiletIdFromUrl();
+console.log(toiletID); // This will log the toilet ID from the URL
+
+// Now you can use toiletID to display reviews or any other related data
+displayReviews(toiletID);
+});
